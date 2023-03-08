425cb2bbea1c541ce4d7be0f590bc9b7276ac2e0
Shona Chan <mojavealoe@gmail.com>
2023-03-08 12:20:50 +0800

    creates Restaurant model and migrates

---
+++ app/models/restaurant.rb
@@ -0,0 +1,11 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian), message: '%{value} is not a valid category' }
+end
+
+# A restaurant must have a name, an address and a category.
+# A restaurant’s category must belong to this fixed list: ["chinese", "italian", "japanese", "french", "belgian"].
+# When a restaurant is destroyed, all of its reviews must be destroyed as well.

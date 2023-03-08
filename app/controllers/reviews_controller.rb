b9894a5a0194420541e5773af0a6d9926dce609e
Shona Chan <mojavealoe@gmail.com>
2023-03-08 16:06:30 +0800

    renders review form with errors

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :set_restaurant, only: %i[new create]
 
-  # GET /restaurants/:restaurant_id/reviews/new
-  def new
-    @review = Review.new
-  end
-
   # POST /restaurants/:restaurant_id/reviews
   def create
     @review = Review.new(review_params)
@@ -13,7 +8,7 @@ class ReviewsController < ApplicationController
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new, status: :unprocessable_entity
+      render 'restaurants/show', status: :unprocessable_entity
     end
   end
 

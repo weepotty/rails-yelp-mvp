4a13a4437a44baefb1ebeb261cec0aa9fb50a5f9
Shona Chan <mojavealoe@gmail.com>
2023-03-08 15:13:54 +0800

    add review functionality

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -25,4 +25,5 @@ class RestaurantsController < ApplicationController
   def restaurant_params
     params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
+
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,29 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: %i[new create]
 
   # GET /restaurants/:restaurant_id/reviews/new
   def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
+  end
+
+  # POST /restaurants/:restaurant_id/reviews
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
